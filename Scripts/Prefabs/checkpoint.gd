c4631b4354d11aa413595f243b57d407c797fcce
ToTM-Dev <robloxit14@gmail.com>
2026-03-05 22:30:33 +0100

    Change : Modifided the check's track on test map 1

--- Scripts/Prefabs/checkpoint.gd
+++ Scripts/Prefabs/checkpoint.gd
@@ -6,16 +6,15 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	print(id)
 	portal.set_layer_mask_value(id + 2, true)
-	print(portal.get_layer_mask_value(id + 2))
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body is RaycastCar:
+	if body is RaycastCar and body.ntc == id:
 		if last:
 			body._change_camera_check_visi(0)
+			body.ntc = 0
 		else:
 			body._change_camera_check_visi(id +1)
+			body.ntc += 1
 		body._change_camera_check_visi(id, false)
